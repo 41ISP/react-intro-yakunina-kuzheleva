b83030667bff18162763352153ead4f8aff13abc
Kera-ux-ui <faernika97@gmail.com>
2026-09-02 09:03:43 +0000

    create function

---
+++ react-intro/src/App.jsx
@@ -0,0 +1,16 @@
+const App = () => {
+   const username = "Kera-ux-ui"
+
+  return(
+    <>
+    <h1> С подключением, {username} </h1>
+    <div>
+      <button>-</button>
+      <span>0</span>
+      <button>+</button>
+    </div>
+    </>
+  )
+}
+
+export default App //вытаскивает переменную
